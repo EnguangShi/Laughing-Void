5a589318bf70775b492b409c399935f2462fc617
EnguangShi <samuelshi20@outlook.com>
2024-10-20 16:35:35 -0700

    Updated langTag

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -61,13 +61,13 @@ const FormattedDatetime = ({ pubDatetime, modDatetime }: DatetimesProps) => {
     modDatetime && modDatetime > pubDatetime ? modDatetime : pubDatetime
   );
 
-  const date = myDatetime.toLocaleDateString(LOCALE.langTag || undefined, {
+  const date = myDatetime.toLocaleDateString(LOCALE.langTag, {
     year: "numeric",
     month: "short",
     day: "numeric",
   });
 
-  const time = myDatetime.toLocaleTimeString(LOCALE.langTag || undefined, {
+  const time = myDatetime.toLocaleTimeString(LOCALE.langTag, {
     hour: "2-digit",
     minute: "2-digit",
   });
